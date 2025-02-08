60ed3ba49dd5af2d1b12664798dda9fd8bafa2e0
josecarlosmeza <75866376+josecarlosmeza@users.noreply.github.com>
2025-02-08 08:28:00 -0300

    Update update.sh

--- update.sh
+++ update.sh
@@ -27,7 +27,7 @@ fun_bar() {
     tput cnorm
 }
 res1() {
-    wget https://raw.githubusercontent.com/Fitunnel/AutoScript/main/menu/menu.zip
+    wget https://raw.githubusercontent.com/josecarlosmeza/AutoScript/main/menu/menu.zip
     unzip menu.zip
     chmod +x menu/*
     mv menu/* /usr/local/sbin
